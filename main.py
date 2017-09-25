88c818b1e5f23a715efa1aa228fe45387cd84759
Tori Klein <torikklein@gmail.com>
2017-09-24 23:29:43 -0500

    Finish web caesar

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
@@ -33,7 +33,7 @@ form = """
         </label>
         <br>
         <label>
-            <textarea name="text"></textarea>
+            <textarea name="text">{0}</textarea>
         </label>
         <br>
         <input type="submit" />
@@ -46,7 +46,7 @@ form = """
 
 @app.route("/")
 def index():
-    return form
+    return form.format(' ')
 
 @app.route("/", methods=['POST'])
 def encrypt():
@@ -55,7 +55,7 @@ def encrypt():
     message = ""
     message = rotate_string(text, rotation)
 
-    return "<h1>" + message + "</h1>"
+    return "<h1>" + form.format(message) + "</h1>"
 
 
 app.run()
